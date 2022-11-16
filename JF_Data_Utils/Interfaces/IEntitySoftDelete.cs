e851c3719a6cbd301bd05adff34811c15ad0a35b
jasonfabian <ye_prog@hotmail.com>
2022-11-15 19:35:49 -0500

    Modified scope Interfaces of entities. Create Interface entity key

--- JF_Data_Utils/Interfaces/IEntitySoftDelete.cs
+++ JF_Data_Utils/Interfaces/IEntitySoftDelete.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JF.Utils.Data.Interfaces
 {
-    internal interface IEntitySoftDelete
+    public interface IEntitySoftDelete
     {
         public DateTime? DeletedDate { get; set; }
 
