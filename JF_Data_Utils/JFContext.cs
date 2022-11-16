85873c1e09199ac540ba8fde61b2bcd04588aefe
jasonfabian <ye_prog@hotmail.com>
2022-11-15 22:30:38 -0500

    Implements Repository Base

--- JF_Data_Utils/JFContext.cs
+++ JF_Data_Utils/JFContext.cs
@@ -2,11 +2,6 @@
 using JF.Utils.Data.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JF.Utils.Data
 {
