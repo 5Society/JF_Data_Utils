90212cda36cd575187c9c1dac13ced6e36070e42
jasonfabian <ye_prog@hotmail.com>
2022-11-23 12:17:24 -0500

    Validation of name and lastname in entity

--- API_JF_Data_Utils_Example/Core/Models/Person.cs
+++ API_JF_Data_Utils_Example/Core/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using JF.Utils.Data;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_JF_Data_Utils_Example.Core.Models
 {
@@ -8,8 +9,14 @@ namespace API_JF_Data_Utils_Example.Core.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MinLength(2, ErrorMessage = "Name cannot be less than 2")]
+        [MaxLength(250, ErrorMessage = "Name cannot be greater than 250")]
+        [Column("Name", TypeName = "varchar")]
         public string Name { get; set; } = "";
         [Required]
+        [MinLength(2, ErrorMessage = "Last name cannot be less than 2")]
+        [MaxLength(250, ErrorMessage = "Last name cannot be greater than 250")]
+        [Column("LastName", TypeName = "varchar")]
         public string LastName { get; set; } = "";
     }
 }
