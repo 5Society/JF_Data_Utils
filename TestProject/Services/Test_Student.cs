c3958bf5b2cdb7e2f03d7e41809d7eee528f0819
jasonfabian <ye_prog@hotmail.com>
2022-11-23 09:31:03 -0500

    Crete nUnit Project

---
+++ TestProject/Services/Test_Student.cs
@@ -0,0 +1,16 @@
+namespace TestProject.Services
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
