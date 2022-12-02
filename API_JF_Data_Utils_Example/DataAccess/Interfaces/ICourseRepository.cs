667d043acb944d8cee7676ae2d86cf53fb71a982
jasonfabian <ye_prog@hotmail.com>
2022-12-01 22:02:46 -0500

    Created Course Controller, Service and Repository

---
+++ API_JF_Data_Utils_Example/DataAccess/Interfaces/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using API_JF_Data_Utils_Example.Core.Models;
+using JF.Utils.Data.Interfaces;
+
+namespace API_JF_Data_Utils_Example.DataAccess.Interfaces
+{
+    public interface ICourseRepository : IRepositoryBase<Course>
+    {
+    }
+}
